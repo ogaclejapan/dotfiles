150134a54c5cd3f54990198d89f8a019f032d644
ogaclejapan <ogaclejapan@gmail.com>
2014-04-27 15:41:46 +0900

    tmuxのプラットフォーム別設定を追加

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -8,6 +8,7 @@ if test -f $HOST_FISH
 else
    echo Creating host fish: $HOST_FISH
    touch $HOST_FISH
+   echo "set -x TMUX_AUTO_ATTACH ''" >> $HOST_FISH
 end
 
 set PLATFORM_FISH ~/.config/fish/(uname -s).fish
@@ -18,12 +19,25 @@ else
    touch $PLATFORM_FISH
 end
 
-if [ -z "$AUTO_TMUX" -a -z "$TMUX" ]
-  tmux has-session ^/dev/null; and tmux list-session | grep -q '^LOCAL'
+set PLATFORM_TMUX ~/.tmux/(uname -s).conf
+if not test -f $PLATFORM_TMUX
+   echo Creating platform tmux conf: $PLATFORM_TMUX
+   touch $PLATFORM_TMUX
+end
+
+set PLATFORM_TMUX_SYMBOLIC ~/.tmux-platform.conf
+if not test -e $PLATFORM_TMUX_SYMBOLIC
+   echo Creating symbolic link: $PLATFORM_TMUX_SYMBOLIC
+   ln -s $PLATFORM_TMUX $PLATFORM_TMUX_SYMBOLIC
+end
+
+set AUTO_TMUX $TMUX_AUTO_ATTACH
+if [ -n "$AUTO_TMUX" -a -z "$TMUX_PANE" ]
+  tmux has-session ^/dev/null; and tmux list-session | grep -q "\^$AUTO_TMUX"
   if [ $status -eq 0 ]
-    tmux attach -t LOCAL
+    tmux attach -t $AUTO_TMUX
   else
-    tmux new -s LOCAL
+    tmux new -s $AUTO_TMUX
   end
 end
 
