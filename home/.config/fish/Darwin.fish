55c765969c04f7782ee37cd90c922a936cef3a6a
ogaclejapan <ogaclejapan@gmail.com>
2014-05-03 22:52:00 +0900

    JAVA_HOMEの定義処理で初期化に失敗したので修正

--- home/.config/fish/Darwin.fish
+++ home/.config/fish/Darwin.fish
@@ -1,7 +1,7 @@
 alias ll='ls -lhF'
 alias la='ls -a'
 
-set -xg JAVA_HOME (/usr/libexec/java_home)
+set -xg JAVA_HOME (echo (command /usr/libexec/java_home))
 
 if test -d ~/.rbenv/shims
 	set -x PATH ~/.rbenv/shims $PATH
