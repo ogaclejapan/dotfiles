1088a29168ef5d3a0d1d0ceeb72efa0d5a056205
ogaclejapan <ogaclejapan@gmail.com>
2016-02-21 12:25:51 +0900

    Add adb-peco

--- home/.config/fish/Darwin.fish
+++ home/.config/fish/Darwin.fish
@@ -24,6 +24,12 @@ if test -d /usr/local/opt/go
   set -x PATH /usr/local/opt/go/libexec/bin $PATH
 end
 
+if test -d ~/.bin/adb-peco
+  set -x PATH ~/.bin/adb-peco/bin $PATH
+  alias adb='adbp'
+  alias pidcat='pidcatp'
+end
+
 if test -d ~/Applications/Atom.app
   set -x ATOM_PATH ~/Applications
 end
