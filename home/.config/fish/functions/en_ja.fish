712bb6261d0efad6c8359111570645e7c44d6ece
ogaclejapan <ogaclejapan@gmail.com>
2016-06-18 10:20:29 +0900

    Add fish functions for translate-shell

---
+++ home/.config/fish/functions/en_ja.fish
@@ -0,0 +1,4 @@
+function en_ja -d 'Translate English into Japanese'
+  trans en:ja -brief $argv
+end
+
---
+++ home/.config/fish/functions/ja_en.fish
@@ -0,0 +1,4 @@
+function ja_en -d 'Translate Japanese into English'
+  trans ja:en -brief $argv
+end
+
