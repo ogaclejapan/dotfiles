9a13e9105e707798438bdfe2119d142a87582797
ogaclejapan <ogaclejapan@gmail.com>
2016-07-13 08:54:43 +0900

    Update host.fish

--- home/.config/fish/host.fish
+++ home/.config/fish/host.fish
@@ -5,6 +5,6 @@ set -x TMUX_AUTO_ATTACH ''
 # JIRAコマンドのベースURL
 set -x JIRA_URL ''
 
-# GitHub Enterpriseのドメイン名
-#set -x GITHUB_HOST 'your-github-enterprise.org'
+# GitHub personal access token for Homebrew
+#set -x HOMEBREW_GITHUB_API_TOKEN 'your personal access token'
 
