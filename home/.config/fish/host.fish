e9410864532b9967d1b33f62268466b10dfd916f
ogaclejapan <ogaclejapan@gmail.com>
2017-06-10 13:42:38 +0900

    Add fzf vars for fish

--- home/.config/fish/host.fish
+++ home/.config/fish/host.fish
@@ -8,3 +8,6 @@ set -x JIRA_URL ''
 # GitHub personal access token for Homebrew
 #set -x HOMEBREW_GITHUB_API_TOKEN 'your personal access token'
 
+# fzf
+set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
+set -x FZF_FIND_FILE_COMMAND $FZF_DEFAULT_COMMAND
