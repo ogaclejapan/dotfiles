1fd914d1c86810b0e4064781f374013bfa262a62
ogaclejapan <ogaclejapan@gmail.com>
2017-12-12 11:59:16 +0900

    Tweak apk function

--- home/.config/fish/functions/apk.fish
+++ home/.config/fish/functions/apk.fish
@@ -1,3 +1,3 @@
 function apk -d 'Find apk from under the current directory'
-  rg -u -g '*.apk' --files | fzf --height=25% --reverse --select-1
+  rg -u -g '*.apk' --files | fzf --min-height=2 --height=25% --reverse --select-1
 end
