2ed0725fa8e747f479da0b3f2229d4b900e330a0
ogaclejapan <ogaclejapan@gmail.com>
2018-01-12 20:40:54 +0900

    Update apk function

--- home/.config/fish/functions/apk.fish
+++ home/.config/fish/functions/apk.fish
@@ -3,5 +3,17 @@ function apk -d 'Find apk from under the current directory'
     echo "fd: not found"
     return 1
   end
-  fd -I -E '.git' -e 'apk' | fzf --min-height=2 --height=25% --reverse --select-1
+
+  set apk (fd -I -E '.git' -e 'apk' | fzf --min-height=2 --height=25% --reverse --select-1)
+  if test -z $apk
+    echo "apk: not found"
+    return 1
+  end
+  
+  if contains -- '-d' $argv
+    dirname $apk
+  else
+    echo $apk
+  end
+    
 end
