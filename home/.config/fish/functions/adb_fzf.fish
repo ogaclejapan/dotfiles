d23d8191b27594bb2c733fa80f261fefc330fe59
ogaclejapan <ogaclejapan@gmail.com>
2018-01-29 08:50:00 +0900

    Fix adb function

--- home/.config/fish/functions/adb_fzf.fish
+++ home/.config/fish/functions/adb_fzf.fish
@@ -26,13 +26,13 @@ function adb_fzf -d 'Execute adb command to interactive if multiple devices conn
 
   switch $argv[1]
     case devices help version start-server kill-server connect disconnect
-      eval $origin $argv
+      eval $origin (string escape -- $argv)
     case '*'
       set t (eval $origin 'devices -l' | sed -e '/^[<space><tab>]*$/d' -e '/^[<space><tab>]*\*.*\*[<space><tab>]*$/d' | fzf --header-lines=1 --select-1 --reverse --min-height=2 --height=25% --prompt='>> ' | tr -s '[:blank:]' '\t' | cut -f 1)
       if test -z $t
         return 0
       else
-        eval $origin '-s' $t $argv
+        eval $origin '-s' $t (string escape -- $argv)
       end
   end
   
