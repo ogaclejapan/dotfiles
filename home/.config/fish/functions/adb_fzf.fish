db8d17498b27ecb29c2d2793ce158d01c9e9392a
ogaclejapan <ogaclejapan@gmail.com>
2018-03-17 10:09:17 +0900

    Update adb function

--- home/.config/fish/functions/adb_fzf.fish
+++ home/.config/fish/functions/adb_fzf.fish
@@ -11,24 +11,116 @@ function adb_fzf -d 'Execute adb command to interactive if multiple devices conn
     end
 
     if test (count $argv) -eq 0
-        command adb (string escape -- $argv)
+        __adb_exec $argv
         return 0
     end
 
     if contains -- -d $argv or contains -- -e $argv or contains -- -s $argv
-        command adb (string escape -- $argv)
+        __adb_exec $argv
         return 0
     end
 
-    switch $argv[1]
+    set -l subcommand $argv[1]
+    set -e argv[1]
+    
+    switch $subcommand
         case devices help version start-server kill-server connect disconnect
-            command adb (string escape -- $argv)
+            __adb_exec $subcommand $argv
+        case screencap
+            __adb_screencapture $argv
+        case screenrecord
+            __adb_screenrecord $argv
         case '*'
-            command adb devices -l | sed -e '/^[<space><tab>]*$/d' -e '/^[<space><tab>]*\*.*\*[<space><tab>]*$/d' | fzf --header-lines=1 --select-1 --reverse --min-height=2 --height=25% --prompt='>> ' | tr -s '[:blank:]' '\t' | cut -f 1 | read -l serial
-            if test -z $serial
-                return 0
-            end
-            command adb -s "$serial" (string escape -- $argv)
+            __adb_exec_with_serial $subcommand $argv
     end
 
 end
+
+function __adb_device
+    command adb devices -l | sed -e '/^[<space><tab>]*$/d' -e '/^[<space><tab>]*\*.*\*[<space><tab>]*$/d' | fzf --header-lines=1 --select-1 --reverse --min-height=2 --height=25% --prompt='>> ' | tr -s '[:blank:]' '\t' | cut -f 1
+end
+
+function __adb_exec
+    command adb (string escape -- $argv)
+end
+
+function __adb_exec_with_serial
+    set serial (__adb_device)
+    if test -z $serial
+        return 0
+    end
+    command adb -s $serial (string escape -- $argv)
+end
+
+function __adb_screencapture
+    set serial (__adb_device)
+    if test -z $serial
+        return 0
+    end
+    set fpath (realpath ~/Desktop)
+    argparse -s 'p/path=' -- $argv
+    if set -q _flag_path
+        set fpath (eval realpath $_flag_path)
+    end
+    set timestamp (date "+%Y%m%d%H%M%S")
+    set fname (printf "screen_%s.png" $timestamp)
+    set tmp (printf "/data/local/tmp/%s" $fname)
+    set dst (printf "%s/%s" $fpath $fname)
+    
+    command adb -s $serial shell screencap -p $tmp
+    and command adb -s $serial pull $tmp $dst > /dev/null
+    and command adb -s $serial shell rm $tmp
+    and echo $dst
+end
+
+function __adb_screenrecord
+    set serial (__adb_device)
+    if test -z $serial
+        return 0
+    end
+    set fpath (realpath ~/Desktop)
+    argparse -s 'p/path=' -- $argv
+    if set -q _flag_path
+        set fpath (eval realpath $_flag_path)
+    end    
+    set timestamp (date "+%Y%m%d%H%M%S")
+    set fname (printf "screen_%s.mp4" $timestamp)
+    set tmp (printf "/data/local/tmp/%s" $fname)
+    set dst (printf "%s/%s" $fpath $fname)    
+
+    command adb -s $serial shell screenrecord $tmp &
+    jobs -lp | grep -v 'Process' | read -l pid
+    if test -z $pid
+        echo "screenrecord: not running" >&2
+        return 1
+    end
+
+    set answer 'n'
+    while test $answer != 'Y'
+        read -P 'Recording, finish? [Y/n]: ' -c 'Y' answer
+    end
+
+    kill -9 $pid
+    
+    set is_running 1
+    while test $is_running -ne 0
+        command adb -s $serial shell ps | grep -c screenrecord | read is_running
+    end
+    
+    command adb -s $serial pull $tmp $dst > /dev/null
+    and command adb -s $serial shell rm $tmp
+    and echo $dst
+end
+
+function __adb_fzf_needs_command
+  set cmd (commandline -opc)
+  if [ (count $cmd) -eq 1 ]
+    return 0
+  else
+    return 1
+  end
+end
+
+complete -f -c adb_fzf -n '__adb_fzf_needs_command' -a screencap -d 'Taking a screenshot of a device display'
+complete -f -c adb_fzf -n '__adb_fzf_needs_command' -a screenrecord -d 'Recording the display of devices running Android 4.4 and higher'
+complete -c adb_fzf -w adb
