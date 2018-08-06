aa724d224b13591ddb384c15f827d353bd0f2a52
ogaclejapan <ogaclejapan@gmail.com>
2018-08-07 08:55:08 +0900

    Disable auto package update for emacs
    When daemon is started, it was blocked by confirmation prompt of
    before update.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -380,13 +380,13 @@
 
 ;; https://github.com/rranelli/auto-package-update.el
 ;; Automatically update Emacs packages.
-(use-package auto-package-update
-  :ensure t
-  :config
-  (setq auto-package-update-delete-old-versions t
-        auto-package-update-interval 30)
-  (auto-package-update-maybe))
-
+;;(use-package auto-package-update
+;;  :ensure t
+;;  :config
+;;  (setq auto-package-update-delete-old-versions t
+;;        auto-package-update-interval 30)
+;;  (auto-package-update-maybe))
+;;
 ;;------------------------------
 
 (custom-set-variables
