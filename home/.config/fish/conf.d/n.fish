e7ec884861a476ebd5f8247369d6380947885ae9
ogaclejapan <ogaclejapan@gmail.com>
2018-08-19 09:00:11 +0900

    Improve version compare for .node-version

--- home/.config/fish/conf.d/n.fish
+++ home/.config/fish/conf.d/n.fish
@@ -4,12 +4,17 @@ function __n_version_on_cd --on-variable PWD  -d 'Automatic node.js version swit
     end
     set -l current_version (node -v | sed -e 's/^v//')
     set -l specified_version (cat .node-version)
-    if test $current_version = $specified_version
+    if __n_version_test $specified_version $current_version
         return 0
     end
-    read --prompt "set_color blue; echo -n '.node-version'; set_color normal; echo ' file found. '; echo -n 'Switch to the specified version? '; set_color green; echo -n '($specified_version)'; set_color normal; echo -n ' [Y/n]: '" --command 'Y' answer
+    read --prompt "set_color blue; echo -n '.node-version'; set_color normal; echo ' file found. must be $specified_version or higher'; echo -n 'Switch to the specified version? '; set_color green; echo -n '($current_version -> $specified_version)'; set_color normal; echo -n ' [Y/n]: '" --command 'Y' answer
     if test $answer != 'Y'
         return 0
     end
     n $specified_version
+end
+
+function __n_version_test -a expected actual -d "Compare versions"
+	printf '%s\n' $expected $actual | sort --check=silent --version-sort
+	return $status
 end
