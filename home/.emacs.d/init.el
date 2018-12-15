06e0e82947481250edf6773a9cf22e6107bb976f
ogaclejapan <ogaclejapan@gmail.com>
2018-12-15 21:48:36 +0900

    Update init.el

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -24,9 +24,6 @@
 (eval-when-compile
   (require 'use-package))
 
-(require 'diminish)
-(require 'bind-key)
-
 ;;--+--+--+--+--+--+--+--+--+--+
 ;; Display Settings
 ;;--+--+--+--+--+--+--+--+--+--+
