ca00987863b36733591556e841cb0e3aeb7e24c6
ogaclejapan <ogaclejapan@gmail.com>
2019-01-06 11:14:02 +0900

    Format init.el

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -392,15 +392,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  '(package-selected-packages
-     (quote
-       (color-theme-sanityinc-tomorrow-theme use-package markdown-mode ace-jump-mode))))
+ '(package-selected-packages
+   (quote
+    (color-theme-sanityinc-tomorrow-theme use-package markdown-mode ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  )
+ )
 
 ;; Local Variables:
 ;; no-byte-compile: t
