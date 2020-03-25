728cbac97e5754aa0f2a53ec6dd380fedb1a4fea
ogaclejapan <ogaclejapan@gmail.com>
2020-03-25 22:51:02 +0900

    Update init.el

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -351,6 +351,15 @@
 
 ;;------------------------------
 
+;; https://github.com/justbur/emacs-which-key
+;; Emacs package that displays available keybindings in popup
+(use-package which-key
+  :ensure t
+  :init
+  (add-hook 'after-init-hook #'which-key-mode))
+
+;;------------------------------
+
 ;; https://github.com/joshwnj/json-mode
 ;; JSON major mode
 (use-package json-mode
@@ -392,9 +401,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (color-theme-sanityinc-tomorrow-theme use-package markdown-mode ace-jump-mode))))
+  '(package-selected-packages
+     (quote
+       (color-theme-sanityinc-tomorrow-theme use-package markdown-mode ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
