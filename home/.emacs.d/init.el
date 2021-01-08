7c107e4d3f4cbf5a558b1ef917b30f07918cac58
ogaclejapan <ogaclejapan@gmail.com>
2021-01-08 13:30:09 +0900

    Update automatic correction

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -403,9 +403,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  '(package-selected-packages
-     (quote
-       (color-theme-sanityinc-tomorrow-theme use-package markdown-mode ace-jump-mode))))
+ '(package-selected-packages
+   '(color-theme-sanityinc-tomorrow-theme use-package markdown-mode ace-jump-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
