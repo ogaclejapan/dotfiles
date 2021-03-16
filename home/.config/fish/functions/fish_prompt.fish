7333b50eeef574e14987b8c4b067de8883d8173b
ogaclejapan <ogaclejapan@gmail.com>
2021-03-16 22:58:33 +0900

    Fix the prompt output error

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,18 +1,26 @@
 function fish_prompt --description 'Write out the prompt'
     # Edit from https://github.com/fish-shell/fish-shell/blob/master/share/functions/fish_prompt.fish
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
     set -l normal (set_color normal)
     set -l color_vcs $fish_color_vcs
 
     set -l color_cwd $fish_color_cwd
     set -l suffix '$'
-    if contains -- $USER root toor
+    if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
             set color_cwd $fish_color_cwd_root
         end
         set suffix '#'
     end
 
-    set -l prompt_status (__fish_print_pipestatus $last_status " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+    set -l bold_flag --bold
+    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+    if test $__fish_prompt_status_generation = $status_generation
+        set bold_flag
+    end
+    set __fish_prompt_status_generation $status_generation
+    set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color $bold_flag $fish_color_status) $last_pipestatus)
 
     echo -n -s (set_color $color_cwd) (prompt_pwd) (set_color $color_vcs) (fish_git_prompt " %s") $normal $prompt_status $suffix " "
 end
