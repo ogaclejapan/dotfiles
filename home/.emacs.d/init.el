8f7e01efa2bbe0d68c94c5ba7531b38ad969a5bb
ogaclejapan <ogaclejapan@gmail.com>
2023-08-06 08:02:54 +0900

    :wrench: Add treesit-auto

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -278,14 +278,6 @@
 
 ;;------------------------------
 
-;; https://github.com/joshwnj/json-mode
-;; JSON major mode
-(use-package json-mode
-  :ensure t
-  :mode ("\\.json\\'" . json-mode))
-
-;;------------------------------
-
 ;; https://github.com/yoshiki/yaml-mode
 ;; Simple major mode to edit YAML file for emacs
 (use-package yaml-mode
@@ -333,5 +325,32 @@
   ;;:hook (after-init . global-company-mode))
 
 ;;------------------------------
+
+;; https://github.com/renzmann/treesit-auto
+;; Automatic installation, usage, and fallback for tree-sitter major modes in Emacs 29
+(use-package treesit-auto
+  :demand t
+  :config
+  (global-treesit-auto-mode))
+
+
+;;--+--+--+--+--+--+--+--+--+--+
+;; Tree-sitter Settings
+;;--+--+--+--+--+--+--+--+--+--+
+
+;; https://archive.casouri.cc/note/2023/tree-sitter-in-emacs-29/index.html
+
+(setq treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")))
+
 
 ;;; init.el ends here
