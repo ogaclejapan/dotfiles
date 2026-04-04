bb451f7211d8b9ddc45ec7626e56de35b7fffd61
ogaclejapan <ogaclejapan@gmail.com>
2026-04-04 21:30:47 +0900

    Update alias

--- home/.config/fish/alias.fish
+++ home/.config/fish/alias.fish
@@ -52,7 +52,7 @@ end
 if type -q claude
     # e.g. gopass env xxx ccmcp
     abbr -a ccmcp --position anywhere 'claude --mcp-config (mcp)'
-    abbr -a ccauto 'claude --permission-mode auto'
+    abbr -a ccauto 'claude --enable-auto-mode'
     abbr -a ccdev 'claude --append-system-prompt "(cat ~/.claude/contexts/dev.md)"'
     abbr -a ccresearch 'claude --append-system-prompt "(cat ~/.claude/contexts/research.md)"'
     abbr -a ccreview 'claude --append-system-prompt "(cat ~/.claude/contexts/review.md)"'
