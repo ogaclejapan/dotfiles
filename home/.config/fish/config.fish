af4c6192360a80035326c824bfc5b58166072c7e
ogaclejapan <ogaclejapan@gmail.com>
2026-05-04 09:40:21 +0900

    Remove unused abbr

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -16,10 +16,6 @@ end
 if type -q claude
     # e.g. gopass env xxx ccmcp
     abbr -a ccmcp --position anywhere 'claude --mcp-config (mcp)'
-    abbr -a ccauto 'claude --enable-auto-mode'
-    abbr -a ccdev 'claude --append-system-prompt "(cat ~/.claude/contexts/dev.md)"'
-    abbr -a ccresearch 'claude --append-system-prompt "(cat ~/.claude/contexts/research.md)"'
-    abbr -a ccreview 'claude --append-system-prompt "(cat ~/.claude/contexts/review.md)"'
 end
 
 if type -q tuicr
